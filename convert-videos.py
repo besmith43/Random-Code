5dbb770e069399e8256447d8e4fb98213fca166c
Blake Smith <besmith43@gmail.com>
2021-04-05 11:42:07 -0500

    added gitignore file and added file copy and error handling in convert-videos.py

--- convert-videos.py
+++ convert-videos.py
@@ -2,8 +2,8 @@
 
 import os
 import subprocess
-import platform
 import datetime
+import shutil
 from dialog import Dialog
 
 def get_files(pwd):
@@ -14,6 +14,12 @@ def get_folders(pwd):
 
 def overwrite(d):
     if d.yesno("Would you like to overwrite the original files?") == d.OK:
+        return True
+    else:
+        return False
+
+def copy(d):
+    if d.yesno("Would you like to copy the original file if there is any errors?") == d.OK:
         return True
     else:
         return False
@@ -32,12 +38,12 @@ def recursive(d):
     else:
         return False
 
-def run_recursively(pwd, overwrite_flag, save_folder):
+def run_recursively(pwd, overwrite_flag, save_folder, copy_if_error=False):
     folders = get_folders(pwd)
 
     for folder in folders:
         if not os.path.exists(save_folder + folder):
-            #os.mkdir(save_folder + folder)
+            os.mkdir(save_folder + folder)
             print("making folder: " + save_folder + folder)
 
         run_recursively(pwd + folder + "/", overwrite_flag, save_folder + folder + "/")
@@ -45,7 +51,7 @@ def run_recursively(pwd, overwrite_flag, save_folder):
     files = get_files(pwd)
 
     for video_file in files:
-        convert_video(video_file, overwrite_flag, pwd, save_folder)
+        convert_video(video_file, overwrite_flag, pwd, save_folder, copy_if_error)
 
 def write_to_file(command, log_path):
     if not os.path.exists(log_path):
@@ -64,13 +70,32 @@ def write_to_file(command, log_path):
     log_file.write(command + " - " + datetime.datetime.now().strftime("%X") + "\n")
     log_file.close()
 
-def convert_video(video_file, overwrite_flag, pwd, save_folder):
+def write_error_to_file(result, log_path):
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    filename = log_path + datetime.datetime.now().strftime("%m_%d_%Y_errors_log.txt")
+
+    print("Log File: " + filename)
+
+    if os.path.exists(filename):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+
+    log_file = open(filename, append_write)
+    log_file.write(" ".join(result.args) + "\n\n" + " ".join(result.stderr) + "\n\n" + " - " + datetime.datetime.now().strftime("%X") + "\n")
+    log_file.close()
+
+def convert_video(video_file, overwrite_flag, pwd, save_folder, copy_if_error=False):
     if video_file[-4:] == ".mp4" or video_file[-4:] == ".mkv":
         new_file = save_folder + video_file[:len(video_file)-4]+"-libx265-slow-qp21"+video_file[-4:]
         command = ["ffmpeg"]
         
         if overwrite_flag:
             command.append("-y")
+        else:
+            command.append("-n")
 
         command.append("-i")
         command.append(pwd + video_file)
@@ -90,7 +115,18 @@ def convert_video(video_file, overwrite_flag, pwd, save_folder):
 
         print("Running the following command\n" + command_str)
         write_to_file(command_str, "/home/besmith/.logs/convert-videos/")
-        #subprocess.run(command)
+        result = subprocess.run(command, capture_output=True, text=True)
+
+        if result.returncode == 1:
+            print("Transcode exited with errors")
+            write_error_to_file(result, "/home/besmith/.logs/convert-videos/")
+            os.remove(new_file)
+            if copy_if_error:
+                print("Copying original to destination")
+                shutil.copy(pwd + video_file, save_folder + video_file)
+                print("Copy complete")
+        else:
+            print("Transcode Completed")
 
 def main():
     d = Dialog(dialog="dialog", autowidgetsize=True)
@@ -98,16 +134,18 @@ def main():
 
     overwrite_flag = overwrite(d)
 
+    copy_if_error = copy(d)
+
     save_folder = save(d)
 
     if recursive(d):
         pwd = os.getcwd() + "/"
-        run_recursively(pwd, overwrite_flag, save_folder)
+        run_recursively(pwd, overwrite_flag, save_folder, copy_if_error)
     else:
         files = get_files(os.getcwd())
 
         for file in files:
-            convert_video(file, overwrite_flag, save_folder)
+            convert_video(file, overwrite_flag, os.getcwd(), save_folder, copy_if_error)
 
 if __name__ == "__main__":
     main()
