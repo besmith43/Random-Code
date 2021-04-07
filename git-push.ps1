26a2dfb8c387b62e102895dc0356b8d88b3dc0fb
Blake Smith <besmith43@gmail.com>
2021-04-07 17:02:14 -0500

    added git-push pwsh script

---
+++ git-push.ps1
@@ -0,0 +1,20 @@
+#!/usr/bin/env pwsh
+
+Param(
+	[Parameter(Position=0)]
+	[string]$UpdatedFiles = "*",
+    [Parameter(Mandatory=$True,Position=1)]
+	[string]$commit_string,
+	[Parameter(Position=2)]
+	[string[]]$Remotes = $(git remote show),
+	[Parameter(Position=3)]
+	[string]$Branch = "master"
+)
+
+git add $UpdatedFiles
+git commit -m $commit_string
+foreach($remote in $Remotes)
+{
+	git pull $remote $Branch
+   	git push $remote $Branch
+}
