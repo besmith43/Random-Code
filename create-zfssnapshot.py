cc9c0db07a13cbc84c5d80d45e378c80476d7548
Blake Smith <besmith43@gmail.com>
2021-05-22 09:02:37 -0500

    saving work

--- create-zfssnapshot.py
+++ create-zfssnapshot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argh
+import subprocess
 
 # the commands to create and list zfs snapshots
 #
@@ -16,7 +17,21 @@ import argh
 # to list zfs pools
 # zfs list
 #
+# to rename zfs snapshots
+# zfs rename volume@snapshot-current-name volume@snapshot-new-name
+#
+# example
+# sudo zfs rename vol1-bulk@backup1 vol1-bulk@backup2
+#
+#
 
+def get_snapshots():
+    snapshots = subprocess.run(['zfs','list','-t','snapshot'],capture_output=True,text=True)
+    snapshots_arr = []
+    for i in range(1,len(snapshots)):
+        snapshots_arr.append(snapshots[i].split()[0])
+
+    return snapshots_arr
 
 def main():
 
